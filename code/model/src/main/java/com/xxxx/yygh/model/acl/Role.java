f8cffad27bc5d2f339d95218e020225aaaa657a8
Sooocen <Sooocen@163.com>
2021-09-02 22:58:34 +0800

    实体类
    项目所有的实体对象(医院 医院设置 科室 排班)的实体 Vo 枚举

---
+++ code/model/src/main/java/com/xxxx/yygh/model/acl/Role.java
@@ -0,0 +1,34 @@
+package com.xxxx.yygh.model.acl;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.xxxx.yygh.model.base.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * <p>
+ * 角色
+ * </p>
+ *
+ * @author Sooocen
+ * @since 2021-08-08
+ */
+@Data
+@ApiModel(description = "角色")
+@TableName("acl_role")
+public class Role extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@ApiModelProperty(value = "角色名称")
+	@TableField("role_name")
+	private String roleName;
+
+	@ApiModelProperty(value = "备注")
+	@TableField("remark")
+	private String remark;
+
+}
+
